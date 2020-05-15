e3dac866a306df0704ad458226c63fd7f7e986e7
Sreeram-2797 <53046999+Sreeram-2797@users.noreply.github.com>
2020-05-15 12:08:33 +0530

    yfiles

---
+++ yfile2.js
@@ -0,0 +1,32 @@
+Jquery source code....
+
+
+function vision(){
+   alert("Vision and strategy are both important to your marketing success. But there is a priority to them. Vision always comes first. If you develop a clear vision, you will attract the right strategy. If you don’t have a clear vision, no strategy will save you.");
+}
+
+function focus(){
+   alert("Nothing matters more to a business than how they make customers feel. How the customer feels determines whether your business survives or sinks. The secret to every businesses growth is word-of-mouth marketing, not the marketing done in the pages of a magazine, newspaper, trade publication, on TV, or other media outlets.")
+}
+
+function media(){
+   alert("The Internet has disrupted the traditional sales process, allowing the prospective customer or client to begin on their own terms via search and social media.") 
+}
+
+function model(){
+   alert("The majority of today’s purchase decisions involved some amount of research online. Your business must be easily found online, easily engaged online, and easily communicated with online. This requires a focus on:")
+}
+
+function educate(){
+   alert("You can educate them through: workshops,seminars,webinars,teleseminar,classes,blogs,newsletters,infogrpahics,slide shares")
+}
+
+function develop(){
+   alert("In the same way that your marketing generates leads, you need to also take that same approach when a prospect wants to learn more.")
+}
+
+function marketcalender(){
+   alert("The one thing that is finite for any business is time. There is always more month than time.")
+}
+
+
